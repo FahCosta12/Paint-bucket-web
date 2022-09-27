28f07042faad2f15fac3a56f8ad757611a8e508d
Fabricio <fabricio_matheus@outlook.com>
2022-09-26 22:27:25 -0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/heading-has-content */
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return(
     <div className="App">
       <h1>Paint bucket</h1>
-      <div className='container'>
-        <div className='glass'>1</div>
+      <div className='Container'>
+        <div className='Glass'></div>
+        <div>
+          <p>You clicked {count} times</p>
+          <button onClick={() => setCount(count + 1)}>
+            Click me
+          </button>
+
+          <p>You clicked {count} times</p>
+          <button onClick={() => setCount(count - 1)}>
+            Click me
+          </button>
+        </div>
       </div>
     </div>
   )
