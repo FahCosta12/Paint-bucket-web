8ec84fcd45dee5eafedd772130373739f6405916
Fabricio <fabricio_matheus@outlook.com>
2022-10-06 23:12:14 -0300

    functioning

--- src/App.js
+++ src/App.js
@@ -1,39 +1,34 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable jsx-a11y/no-access-key */
 /* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/heading-has-content */
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return(
-    <div class="App">
+  return (
+    <div className="App">
       <h1>Paint bucket</h1>
-      <div class='Container'>
-        <div class='Glass'>Glass</div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-        <div>
-          <button 
-            class='decrement'
-            onClick={() => setCount(count - 1)}
-            >
-            -
-          </button>
-          
-          <button
-            class='increment'
-            onClick={() => setCount(count + 1)}
-            >
-            +
-          </button>
+      <div className="Container">
+        <button className="decrement" onClick={() => setCount(count - 1)}>
+          -
+        </button>
+        {count}
+        <div className="Glass">
+        {count >= 1 && <div className="Nivel-1">1</div>}
+        {count >= 2 && <div className="Nivel-2">2</div>}
+        {count >= 3 && <div className="Nivel-3">3</div>}
+        {count >= 4 && <div className="Nivel-4">4</div>}
         </div>
+
+        <button className="increment" onClick={() => setCount(count + 1)}>
+          +
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
